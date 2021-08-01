d3ebe2dcf1612987073887ba9107b86575ede0c5
Tetiana Rabiievska <tetiana@careerfoundry.com>
2021-08-01 17:02:20 +0200

    Added html for the timer and buttons, added variables

--- src/index.js
+++ src/index.js
@@ -2,4 +2,47 @@ window.addEventListener("load", function () {
   document.querySelector('#name').addEventListener("keyup", e => {
     document.querySelector('#greet').innerHTML = 'Hello ' + document.querySelector('#name').value;
   });
+
+  const pomodoroTimer = document.querySelector('#pomodoro-timer');
+  const startButton = document.querySelector('#pomodoro-start');
+  const pauseButton = document.querySelector('#pomodoro-pause');
+  const stopButton = document.querySelector('#pomodoro-stop');
+
+  let isClockRunning = false;
+
+  // in seconds = 25 mins
+  let workSessionDuration = 1500;
+  let currentTimeLeftInSession = 1500;
+  // in seconds = 5 mins;
+  let breakSessionDuration = 300;
+
+  // START
+  startButton.addEventListener('click', () => {
+    toggleClock();
+  });
+
+  // PAUSE
+  pauseButton.addEventListener('click', () => {
+    toggleClock();
+  });
+
+  // STOP
+  stopButton.addEventListener('click', () => {
+    toggleClock(true);
+  });
+
+  const toggleClock = (reset) => {
+    if (reset) {
+      // STOP THE TIMER
+    } else {
+      if (isClockRunning === true) {
+        // PAUSE THE TIMER
+        isClockRunning = false
+      } else {
+        // START THE TIMER
+        isClockRunning = true
+      }
+    }
+  }
+  
 });
