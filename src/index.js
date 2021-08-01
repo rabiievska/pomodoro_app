8c580e499b8a35c27349c7946baddc9e2a88dae7
Tetiana Rabiievska <tetiana@careerfoundry.com>
2021-08-01 18:24:25 +0200

    Removed the old code, added 25 min timer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 window.addEventListener("load", function () {
-  document.querySelector('#name').addEventListener("keyup", e => {
-    document.querySelector('#greet').innerHTML = 'Hello ' + document.querySelector('#name').value;
-  }); 
   
   const pomodoroTimer = document.querySelector('#pomodoro-timer');
   const startButton = document.querySelector('#pomodoro-start');
@@ -16,37 +13,53 @@ window.addEventListener("load", function () {
   // in seconds = 5 mins;
   let breakSessionDuration = 300;
 
-  // START
-  startButton.addEventListener('click', () => {
-    toggleClock();
-  });
+  // // START
+  // startButton.addEventListener('click', () => {
+  //   toggleClock();
+  // });
 
-  // PAUSE
-  pauseButton.addEventListener('click', () => {
-    toggleClock();
-  });
+  // // PAUSE
+  // pauseButton.addEventListener('click', () => {
+  //   toggleClock();
+  // });
 
-  // STOP
-  stopButton.addEventListener('click', () => {
-    toggleClock(true);
-  });
+  // // STOP
+  // stopButton.addEventListener('click', () => {
+  //   toggleClock(true);
+  // });
 
-  const toggleClock = (reset) => {
-    if (reset) {
-      // STOP THE TIMER
-    } else {
-      if (isClockRunning === true) {
-        // PAUSE THE TIMER
-        clearInterval(clockTimer);
-        isClockRunning = false;
-      } else {
-        // START THE TIMER
-        clockTimer = setInterval(() => {
-          // decrease time left / increase time spent
-          currentTimeLeftInSession -= 1;
-        }, 1000);
-        isClockRunning = true;
-      }
+  // const toggleClock = (reset) => {
+  //   if (reset) {
+  //     // STOP THE TIMER
+  //   } else {
+  //     if (isClockRunning === true) {
+  //       // PAUSE THE TIMER
+  //       clearInterval(clockTimer);
+  //       isClockRunning = false;
+  //     } else {
+  //       // START THE TIMER
+  //       clockTimer = setInterval(() => {
+  //         // decrease time left / increase time spent
+  //         currentTimeLeftInSession -= 1;
+  //       }, 1000);
+  //       isClockRunning = true;
+  //     }
+  //   }
+  // };
+
+  
+
+  setInterval(() => {
+    const secondsLeft = currentTimeLeftInSession--;
+
+    const seconds = secondsLeft % 60;
+    const minutes = parseInt(secondsLeft / 60) % 60;
+
+    function addLeadingZeroes(time) {
+      return time < 10 ? `0${time}` : time;
     }
-  };
+    result = `${addLeadingZeroes(minutes)}:${addLeadingZeroes(seconds)}`;
+    pomodoroTimer.innerText = result.toString();
+  }, 1000);
+  
 });
