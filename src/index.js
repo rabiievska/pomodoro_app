fb60f8ffe91f0704979b0182a5155b9389c976e2
Tetiana Rabiievska <tetiana@careerfoundry.com>
2022-02-04 16:37:43 +0100

    Added backround script

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function () {
+// window.addEventListener("load", function () {
 
   let countdown = 0; // variable to set/clear intervals
   let seconds = 10; // seconds left on the clock
@@ -111,4 +111,4 @@ window.addEventListener("load", function () {
   };
 
   timerEvents();
-});
+// });
